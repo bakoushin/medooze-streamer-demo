5122d19e5eca1b4a78c1ee09a54c29fa275ea40b
Alex Bakoushin <alex@bakoush.in>
2020-05-14 21:30:28 +0300

    Implement GStreamer MP4 recording pipeline

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const { spawn } = require('child_process');
 const MediaServer = require('medooze-media-server');
-const { SDPInfo } = require('semantic-sdp');
+const { SDPInfo, MediaInfo, CodecInfo } = require('semantic-sdp');
 const internalIp = require('internal-ip');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -15,6 +16,14 @@ capabilities.video.codecs = ['h264;packetization-mode=1'];
 
 // Variable for storing ref to incoming stream
 let incomingStream;
+
+// Variable for storing ref to working GStreamer process
+let gstreamerProcess;
+
+// IP and ports for streaming to
+const STREAMER_REMOTE_IP = '127.0.0.1';
+const STREAMER_AUDIO_PORT = 5004;
+const STREAMER_VIDEO_PORT = 5006;
 
 // Init HTTP server
 const app = express();
@@ -49,6 +58,103 @@ app.post('/connect', (req, res) => {
     type: 'answer',
     sdp: answer.unify().toString(),
   });
+});
+
+// Start recording
+app.get('/recorder-start', () => {
+  // Spawn GStreamer process which will listen to RTP stream from MediaServer.
+  // GStreamer is set up to mux H264 video stream with AAC audio stream into a single MP4 file.
+  // Since WebRTC normally uses Opus as audio codec, it will be transcoded into AAC by GStreamer.
+  gstreamerProcess = spawn(
+    'gst-launch-1.0',
+    [
+      '--eos-on-shutdown',
+      `mp4mux faststart=true name=mux ! filesink location=${Date.now()}.mp4`,
+      `udpsrc address=${STREAMER_REMOTE_IP} port=${STREAMER_AUDIO_PORT} caps="application/x-rtp,clock-rate=48000,payload=109" name=audio`,
+      'audio. ! queue ! rtpjitterbuffer ! rtpopusdepay ! opusdec ! audioconvert ! audioresample ! avenc_aac ! aacparse ! mux.',
+      `udpsrc address=${STREAMER_REMOTE_IP} port=${STREAMER_VIDEO_PORT} caps="application/x-rtp,clock-rate=90000,payload=96" name=video`,
+      'video. ! queue ! rtpjitterbuffer ! rtph264depay ! h264parse ! mux.',
+    ]
+      .join(' ')
+      .split(' ')
+  );
+  console.log('GStreamer started');
+
+  // Wait for GStreamer to initialize and start MediaServer Streamer
+  gstreamerProcess.stdout.on('data', (chunk) => {
+    chunk
+      .toString()
+      .split(/\r?\n/g)
+      .forEach((line) => {
+        if (line === 'New clock: GstSystemClock') {
+          // Create new Streamer
+          streamer = MediaServer.createStreamer();
+
+          // Audio stream
+
+          // Start audio stream
+          const audio = new MediaInfo('audio', 'audio');
+          audio.addCodec(new CodecInfo('opus', 109));
+
+          // Create StreamerSession for audio
+          streamerSessionAudio = streamer.createSession(audio, {
+            remote: {
+              ip: STREAMER_REMOTE_IP,
+              port: STREAMER_AUDIO_PORT,
+            },
+          });
+
+          // Attach audio track from incoming stream to streamer session
+          streamerSessionAudio
+            .getOutgoingStreamTrack()
+            .attachTo(incomingStream.getAudioTracks()[0]);
+
+          // Video stream
+
+          // Create codec description
+          const video = new MediaInfo('video', 'video');
+          video.addCodec(new CodecInfo('h264', 96));
+
+          // Create StreamerSession for video
+          streamerSessionVideo = streamer.createSession(video, {
+            remote: {
+              ip: STREAMER_REMOTE_IP,
+              port: STREAMER_VIDEO_PORT,
+            },
+          });
+
+          // Attach video track from incoming stream to streamer session
+          streamerSessionVideo
+            .getOutgoingStreamTrack()
+            .attachTo(incomingStream.getVideoTracks()[0]);
+
+          console.log('Recording started');
+        }
+      });
+  });
+
+  gstreamerProcess.on('exit', (code, signal) => {
+    console.log(`GStreamer stopped with exit code ${code} (${signal})`);
+
+    // Stop streamer
+    streamerSessionVideo.stop();
+    streamerSessionAudio.stop();
+    streamer.stop();
+    console.log('Streamer stopped');
+  });
+
+  gstreamerProcess.on('error', (err) => {
+    console.error('GStreamer error:', err);
+  });
+
+  gstreamerProcess.stdout.pipe(process.stdout);
+  gstreamerProcess.stderr.pipe(process.stderr);
+});
+
+// Stop recording
+app.get('/recorder-stop', () => {
+  gstreamerProcess.kill('SIGINT');
+  console.log('GStreamer stopped');
 });
 
 // Start HTTP server
